f542c0206e3355bf0f3dbc7fa31883a5d5ad5059
AuroraConst <auroraconstellations@gmail.com>
2025-03-30 14:05:19 -0400

    Update cli-util.ts
    added parse util and exporting cstutils, astutils

--- src/cli/cli-util.ts
+++ src/cli/cli-util.ts
@@ -7,6 +7,12 @@ import { PCM } from '../language/generated/ast.js';
 import { NodeFileSystem } from 'langium/node';
 import { createAuroraServices } from '../language/aurora-module.js';
 
+/**makes thes utils usable from ScalablyTyped perspective */
+import { CstUtils,AstUtils} from 'langium'
+export {CstUtils,AstUtils}
+
+
+/** utility function parse a filename to PCM */
 export const parse = async (fileName: string): Promise<PCM> => {
     const services = createAuroraServices(NodeFileSystem).Aurora;
     const module = await extractAstNode<PCM>(fileName, services);
