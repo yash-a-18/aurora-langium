b4b7c8897072db9e5249dbfb2a1f59f62c0843e2
yash-a-18 <yashamethiya2001@gmail.com>
2025-10-22 11:14:53 -0400

    Update version to 0.3.2 and enhance service creation with EmptyFileSystem

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -11,6 +11,7 @@ export { AstUtils};
 import { createAuroraServices } from '../language/aurora-module.js';
 import { extractAstNode } from './cli-util.js';
 import { NodeFileSystem } from 'langium/node';
+import { EmptyFileSystem } from 'langium';
 // import type { PCM} from '../language/generated/ast.js';
 
 /** utility function parse a filename to PCM */
@@ -21,3 +22,14 @@ export const parse = async (fileName: string): Promise<PCM> => {
     return module;
 };
 
+export const getAuroraServices = async () => {
+    const services = await Promise.resolve(createAuroraServices(NodeFileSystem));
+    return services;
+};
+
+export const getEmptyAuroraServices = async () => {
+    const services = await Promise.resolve(createAuroraServices(EmptyFileSystem));
+    return services;
+}
+
+export { extractAstNode };
