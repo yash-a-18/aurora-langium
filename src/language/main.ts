9dda97996c59423cec2029e1a30ed07a5be64a55
saki-aurora <skaushi1@lakeheadu.ca>
2026-03-08 01:30:55 -0500

    feat: upgrade base to langium v4, testing succeeds

--- src/language/main.ts
+++ src/language/main.ts
@@ -2,30 +2,26 @@ import { startLanguageServer } from 'langium/lsp';
 import { NodeFileSystem } from 'langium/node';
 import { createConnection, ProposedFeatures } from 'vscode-languageserver/node.js';
 import { createAuroraServices } from './aurora-module.js';
-import { addDiagramHandler } from 'langium-sprotty'
+import { addDiagramHandler, type LangiumSprottySharedServices } from 'langium-sprotty';
 
-// Create a connection to the client
 const connection = createConnection(ProposedFeatures.all);
 
-// Add error handling
-connection.console.error = (error) => {
+connection.console.error = error => {
     console.error('Connection error:', error);
 };
 
-process.on('uncaughtException', (error) => {
+process.on('uncaughtException', error => {
     console.error('Uncaught exception:', error);
     connection.console.error(`Uncaught exception: ${error.message}`);
 });
 
 try {
-    // Inject the shared services and language-specific services
     const { shared } = createAuroraServices({ connection, ...NodeFileSystem });
 
-    // Start the language server with the shared services
     console.log('Starting language server...');
     startLanguageServer(shared);
+    addDiagramHandler(connection, shared as LangiumSprottySharedServices);
     console.log('Language server started!!!!!!...');
-    addDiagramHandler(connection, shared);
 } catch (error) {
     if (error instanceof Error) {
         console.error('Failed to start language server:', error);
