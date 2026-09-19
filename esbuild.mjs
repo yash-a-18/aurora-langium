f42fdd1e71ad4a1a8d878d271b831c63e312383a
lsikki <108148866+lsikki@users.noreply.github.com>
2026-09-19 13:12:21 -0400

    reverting old commits to bring aurora-langium up to speed

--- esbuild.mjs
+++ esbuild.mjs
@@ -31,9 +31,7 @@ const cjsContext = await esbuild.context({
     entryPoints: ['src/extension/main.ts', 
         'src/language/main.ts','src/extension/langclientconfig.ts', 
         'src/cli/index.ts', 'src/extension/src/parser/parser.ts',
-        'src/language/aurora-module.ts', 'src/extension/src/commands/toggle-diagram-layout-command.ts',
-        'src/language/aurora-diagram-generator.ts', 'src/extension/src/commands/hide-narratives-command.ts',
-        'src/extension/src/commands/hide-ngos-command.ts'
+        'src/language/aurora-module.ts'
     ],
     outdir: 'dist/cjs', // Output directory for CommonJS
     bundle: true,
@@ -54,9 +52,7 @@ const cjsContext = await esbuild.context({
 const esmContext = await esbuild.context({
     entryPoints: ['src/extension/main.ts', 'src/language/main.ts',
         'src/extension/langclientconfig.ts',  'src/cli/main.ts', 'src/extension/src/parser/parser.ts',
-        'src/language/aurora-module.ts', 'src/extension/src/commands/toggle-diagram-layout-command.ts',
-        'src/language/aurora-diagram-generator.ts', 'src/extension/src/commands/hide-narratives-command.ts',
-        'src/extension/src/commands/hide-ngos-command.ts'
+        'src/language/aurora-module.ts'
     ],
     outdir: 'dist/esm', // Output directory for ES Module
     bundle: true,
